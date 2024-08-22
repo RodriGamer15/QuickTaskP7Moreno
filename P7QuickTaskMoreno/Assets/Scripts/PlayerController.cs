72970337f50f993cb956ba8760a651e654787da7
Rodrigo Moreno <145504670+RodriGamer15@users.noreply.github.com>
2024-08-22 15:28:09 -0700

    Updated the project
    I updated the project by adding a player sprite, circle placeholder platforms, and a player script.

---
+++ P7QuickTaskMoreno/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+    }
+}
