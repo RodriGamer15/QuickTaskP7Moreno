afb3c8bddc28591279f24861a7e1415093b56984
Rodrigo Moreno <145504670+RodriGamer15@users.noreply.github.com>
2024-08-26 15:29:35 -0700

    Added more elements
    I added more to the script and added code to make the player move and jump

--- P7QuickTaskMoreno/Assets/Scripts/PlayerController.cs
+++ P7QuickTaskMoreno/Assets/Scripts/PlayerController.cs
@@ -1,25 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rigidbody2d;
-    float horizontal;
+    Rigidbody2D rb;
     float vertical;
+    float moveSpeed = 5;
+    float jumpForce = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
+        float horizontal = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        transform.Translate(Vector2.right * horizontal);
         vertical = Input.GetAxis("Vertical");
 
-        Vector2 move = new Vector2(horizontal, vertical);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+        }
     }
 }
